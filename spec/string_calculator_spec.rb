19f1af4fef1f01b6a78659bfa815bd7ea8cda925
Øyvind Guttvik Årnes <oyvind.arnes@bouvet.no>
2024-02-02 23:50:51 +0100

    Completed 1.3, Added parameter info to rspec context

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,14 +2,19 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator, '#add(numbers)' do
   string_calculator = StringCalculator.new
-  context 'with single number' do
-    it 'return the same number as int' do
+  context "with single number as parameter: '1'" do
+    it 'return the same number as int: 1' do
       expect(string_calculator.add('1')).to eq 1
     end
   end
-  context 'with two comma separated numbers' do
-    it 'return the sum of the numbers' do
+  context "with two comma separated numbers as parameter: '1,4'" do
+    it "return the sum of the numbers as int: 5" do
       expect(string_calculator.add('1,4')).to eq 5
+    end
+  end
+  context "with empty string as parameter: '' " do
+    it 'return 0' do
+      expect(string_calculator.add('')).to eq 0
     end
   end
 end
