ac4a78b20e60cb079962f9dc9946c415382f773a
Øyvind Guttvik Årnes <oyvind.arnes@bouvet.no>
2024-02-05 22:32:52 +0100

    refactoring

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,39 +1,62 @@
 class StringCalculator
   def add(numbers)
-    # find additonal split character in first line of parameter
-    split_regexp = /,|\\n/
-    if numbers.start_with?('//')
-      split_array = numbers.split('\\n')
-      custom_split_character = Regexp.escape(split_array.first[-1])
-      custom_split_words = split_array.first.scan(/\[(.*?)\]/)
-      split_regexp = /,|\\n|#{custom_split_character}#{custom_split_words.flatten.collect{|w| "|#{Regexp.escape(w)}"}.join}/
-      numbers = split_array.last
-    end
 
+    return 0 if numbers.empty?
+    
+    # Extract possible custom delimiter character and words from first line in 'numbers'
+    custom_delimiters, numbers = extract_custom_delimiters(numbers)
+
+    # Extract possible numbers that are enclosed in exclamation marks. They are exempt from paying big number tax !555!.
+    tax_exempt, numbers_array = extract_numbers(numbers, custom_delimiters)
+
+    # Numbers larger than 1000 are ignored
+    numbers_array.reject!{|n| n > 1000}
+
+    # Check for negative numbers and raise an exception if any
+    validate_negative_numbers(numbers_array)
+
+    apply_tax_and_sum(numbers_array, tax_exempt)
+  end
+
+  private
+
+  def extract_custom_delimiters(numbers)
+    if numbers.start_with?('//')
+      split_array = numbers.split('\\n', 2)
+      custom_delimiter = Regexp.escape(split_array.first[2])
+      custom_delimeter_words = split_array.first.scan(/\[(.*?)\]/).flatten
+      numbers = split_array.last
+      [/,|\\n|#{custom_delimiter}#{custom_delimeter_words.collect{|w| "|#{Regexp.escape(w)}"}.join}/, numbers]
+    else
+      [/,|\\n/, numbers]
+    end
+  end
+
+  def extract_numbers(numbers, delimiters)
     tax_exempt = []
-    numbers_array = numbers.split(split_regexp).collect do |number|
+    numbers_array = numbers.split(delimiters).collect do |number|
       if number.start_with?('!') && number.end_with?('!')
-        tax_exempt << number[1..-1].to_i
-        number[1..-1].to_i
+        tax_exempt << number[1..-2].to_i
+        number[1..-2].to_i
       else
         number.to_i
       end
     end
+    [tax_exempt, numbers_array]
+  end
 
-    numbers_array.reject!{|n| n > 1000}
-
-    negative_numbers = numbers_array.select{|n| n < 0}
+  def validate_negative_numbers(numbers_array)
+    negative_numbers = numbers_array.select { |n| n < 0 }
     raise ArgumentError, "Negative numbers are not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
-    
-    # numbers greater than 99 that are not exempt have to pay tax
-    numbers_array.collect! do |number|
+  end
+
+  def apply_tax_and_sum(numbers_array, tax_exempt)
+    numbers_array.collect do |number|
       if tax_exempt.include?(number)
         number
       else
-        number - number/100
+        number - number / 100
       end
-    end
-
-    numbers_array.collect{|n| n.to_i}.sum
+    end.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,16 +1,16 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator, '#add(numbers)' do
-  string_calculator = StringCalculator.new
+  let(:string_calculator) { StringCalculator.new }
   
-  context "with single number as parameter: '1'" do
-    it 'return the same number as int: 1' do
+  context "with single number" do
+    it 'return the number itself' do
       expect(string_calculator.add('1')).to eq 1
     end
   end
   
-  context "with two comma separated numbers as parameter: '1,4'" do
-    it "return the sum of the numbers as int: 5" do
+  context "with two comma separated numbers" do
+    it "return the sum of the numbers" do
       expect(string_calculator.add('1,4')).to eq 5
     end
   end
@@ -21,59 +21,56 @@ RSpec.describe StringCalculator, '#add(numbers)' do
     end
   end
   
-  context "with any number of numbers as parameter: '3,16,200,5'" do
-    it "returns the sum of the numbers minus the tax for 200(2): 222" do
+  context "with multiple comma separated numbers" do
+    it "returns the sum of the numbers minus the tax for numbers lager than 99" do
       expect(string_calculator.add('3,16,200,5')).to eq 222
     end
   end
   
-  context "with both newline and comma as separator: '45,16\\n33\\n2'" do
-    it "return the sum of the numbers: 96" do
+  context "with numbers separated by both comma and newline" do
+    it "return the sum of the numbers" do
       expect(string_calculator.add('45,16\n33\n2')).to eq 96
     end
   end
   
-  context "with negative numbers as parameter: '-1,2,-3'" do
-    it "throws an exception containing the negative numbers: '-1,-3'" do
+  context "with negative numbers" do
+    it "throws an exception containing the negative numbers:" do
       expect{string_calculator.add('-1,2,-3')}.to raise_error(ArgumentError, /^(?=.*-1)(?=.*-3).*/)
     end
   end
 
-  context "with numbers larger than 1000: '1,1001'" do
+  context "with numbers larger than 1000" do
     it "inores them and returns the sum of the others: 1" do
       expect(string_calculator.add('1,1001')).to eq 1
     end
   end
 
   context "with numbers larger than 99" do
-    it "they have to 'pay tax' for their hundreds: '101' -> 100, '375,4' -> 376, '1000' -> 990" do
+    it "they have to pay large number tax equal to their hundreds didgit" do
       expect(string_calculator.add('101')).to eq 100
       expect(string_calculator.add('375,4')).to eq 376
       expect(string_calculator.add('1000')).to eq 990
     end
   end
 
-  context "with first line starting with // containing custom separator character: '//;\\n1;3" do
-    it "this sign character also be used to split the numbers: 4" do
-      expect(string_calculator.add('//;\n1;3')).to eq 4
-    end
-  end
-
-  context "with exclamation marks surrounding numbers: '1,!999!'" do
-    it "they do not 'pay tax': 1000" do
+  context "with exclamation marks encapsulating numbers" do
+    it "they are exempt from paying large number tax" do
       expect(string_calculator.add('1,!999!')).to eq 1000
     end
   end
 
-  context "with custom separator words surrounded by [] in first line: '//[*x*]\\n1*x*2*x*3'" do
-    it "should also be used as a separator: 6" do
+  context 'with custom delimiters' do
+    it 'handles custom character delimiter' do
+      expect(string_calculator.add('//;\n1;3')).to eq 4
+    end
+
+    it 'handles custom word delimiter' do
       expect(string_calculator.add('//[*x*]\n1*x*2*x*3')).to eq 6
     end
-  end
 
-  context "with multiple separator words: ('//[xyz][$][**]\\n1$5xyz**2'" do
-    it "should use all separators: 8" do
+    it 'handles multiple custom delimiters' do
       expect(string_calculator.add('//[xyz][$][**]\n1$5xyz**2')).to eq 8
     end
   end
+
 end
